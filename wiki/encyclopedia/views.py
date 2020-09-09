df6003b37013638a9709fd00c3633b334afd8e9a
W van der Linden <wicus.vanderlinden@gmail.com>
2020-09-09 10:51:05 +0200

    Entry, Index, Random, New

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,55 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+import markdown2
+import random
+from django import forms
 
 from . import util
 
+class NewEntryForm(forms.Form):
+    title = forms.CharField(label="Title")
+    body = forms.CharField(label="Body")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+    
+def entry(request, entry):
+    body_md = util.get_entry(entry)
+    body = None
+    if body_md is not None:
+        body = markdown2.markdown(body_md)
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "code": 0, "entry": entry})
+    return render(request, "encyclopedia/entry.html", {
+        "entry": entry, "body": body})
+        
+def newpage(request):
+    if request.method == "POST":
+        title = request.POST["title"]
+        content = request.POST["body"]
+        entries = util.list_entries()
+        if title in entries:
+            return render(request, "encyclopedia/error.html", {
+                "code": 1, "entry": title})
+        else:
+            util.save_entry(title, content)
+            body = markdown2.markdown(content)
+            return render(request, "encyclopedia/entry.html", {
+                "entry": title, "body": body})
+    return render(request, "encyclopedia/newpage.html", {
+        "form": NewEntryForm()})
+    
+def randompage(request):
+    entries = util.list_entries()
+    nr_entries = len(entries)
+    rand = random.randint(0, nr_entries - 1)
+    entry = entries[rand]
+    body_md = util.get_entry(entry)
+    body = markdown2.markdown(body_md)
+    return render(request, "encyclopedia/entry.html", {
+        "entry": entry, "body": body}) 
 
